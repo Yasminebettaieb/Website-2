4c94fa4dc164a120b4f2c9b3498d2ff9a5fa5402
Yasminebettaieb <71673006+Yasminebettaieb@users.noreply.github.com>
2022-02-01 10:17:50 +0000

    format

--- js/map-api.js
+++ js/map-api.js
@@ -43,19 +43,44 @@ function locationChangeHandler(autoc,autoc1){
 (function($){
     
     
-
-    $("#from-date-picker").datetimepicker({
+    
+ $("#from-date-picker").datetimepicker({
         
         controlType: 'select',
         oneLine: true,
-        timeFormat: 'HH:mm', 
+        timeFormat: 'HH:mm',
         onClose: function(dateText) {
             PriceCalculator();
-        }
+        },
         
-    }).setLocale('fr'); 
+        
+        
+    });
+    $.datepicker.regional['fr'] = {clearText: 'Effacer', clearStatus: '',
+    closeText: 'Fermer', closeStatus: 'Fermer sans modifier',
+    prevText: '<Préc', prevStatus: 'Voir le mois précédent',
+    nextText: 'Suiv>', nextStatus: 'Voir le mois suivant',
+    currentText: 'Courant', currentStatus: 'Voir le mois courant',
+    monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin',
+    'Juillet','Août','Septembre','Octobre','Novembre','Décembre'],
+    monthNamesShort: ['Jan','Fév','Mar','Avr','Mai','Jun',
+    'Jul','Aoû','Sep','Oct','Nov','Déc'],
+    
+    monthStatus: 'Voir un autre mois', yearStatus: 'Voir un autre année',
+    weekHeader: 'Sm', weekStatus: '',
+    dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
+    dayNamesShort: ['Dim','Lun','Mar','Mer','Jeu','Ven','Sam'],
+    dayNamesMin: ['Di','Lu','Ma','Me','Je','Ve','Sa'],
+    dayStatus: 'Utiliser DD comme premier jour de la semaine', dateStatus: 'Choisir le DD, MM d',
+    dateFormat: 'dd/mm/yy', firstDay: 0, 
+    initStatus: 'Choisir la date', isRTL: false, currentText:"Maintenant",
+    closeText:"Done",amNames:["AM","A"],pmNames:["PM","P"],timeFormat:"HH:mm",
+    timeSuffix:"",timeOnlyTitle:"Choose Time",timeText:"Time",hourText:"Hour",minuteText:"Minute",
+    secondText:"Second",millisecText:"Millisecond",microsecText:"Microsecond",timezoneText:"Time Zone"};
+ $.datepicker.setDefaults($.datepicker.regional['fr']);
+
  
-  
+
 })(jQuery);
 
 function PriceCalculator (){
